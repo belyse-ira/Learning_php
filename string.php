16e767beff27ffe6bc16e4739ba67aac89932d89
belyse-ira <irasubizabelyse12@gmail.com>
2025-04-13 13:05:13 +0200

    add string.php

---
+++ string.php
@@ -0,0 +1,21 @@
+<!DOCTYPE>
+<html>
+    <head>
+        <meta charset="UTF-8">
+</head>
+<body>
+<?php
+$sentence="The body of a book";
+echo $sentence;
+echo strtolower($sentence);
+echo $sentence;
+echo strtoupper($sentence);
+echo $sentence;
+echo str_replace("book","college");
+echo $sentence;
+echo strlen($sentence);
+echo ($sentence);
+echo "m";
+?>
+</body>
+</html>
